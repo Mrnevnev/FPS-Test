1a5287242bd0e04aeafbfffdc868003980cd301f
NevillAdeyeye <80149156+Mrnevnev@users.noreply.github.com>
2025-04-07 21:22:08 -0700

    Laptop Commit.4.7.25
    Got the enemy to follow and stop at a certain range. They will also look at you once you get into a certain range.

--- FPS Test/Assets/Scripts/EnemyController.cs
+++ FPS Test/Assets/Scripts/EnemyController.cs
@@ -9,10 +9,17 @@ public class EnemyController : MonoBehaviour
     public float moveSpeed;
 
     public Rigidbody rb;
+
+    public float chaseRange = 15f, stopChasingRange = 5f;
+
+    private float strafeAmount;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         _player = FindAnyObjectByType<PlayerController>();
+
+        strafeAmount = Random.Range(-.75f, .75f);
     }
 
     // Update is called once per frame
@@ -20,11 +27,30 @@ public class EnemyController : MonoBehaviour
     {
 
         float yStore = rb.linearVelocity.y;
-        
-        transform.LookAt(new Vector3(_player.transform.position.x, transform.position.y, _player.transform.position.z));
-        
-        rb.linearVelocity = transform.forward * moveSpeed;
 
+        float distance = Vector3.Distance(transform.position, _player.transform.position);
+
+        if (distance < chaseRange)
+        {
+            transform.LookAt(new Vector3(_player.transform.position.x, transform.position.y, _player.transform.position.z));
+            
+            if (distance > stopChasingRange )
+            {
+                rb.linearVelocity = (transform.forward + (transform.right * strafeAmount)) * moveSpeed;
+                
+            }
+            else
+            {
+                rb.linearVelocity = Vector3.zero;
+            }
+        }
+        else
+        {
+            rb.linearVelocity = Vector3.zero;
+        }
+        
+        
+        
         rb.linearVelocity = new Vector3(rb.linearVelocity.x, yStore, rb.linearVelocity.z);
     }
 }
